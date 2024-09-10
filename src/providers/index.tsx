7834351cff8a5af378f9d6def398231ff0c554f8
Daniel Boll <danielboll.academico@gmail.com>
2024-09-10 15:54:10 -0300

    feat(ui): add dark mode toggle provider through `next-themes`
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

---
+++ src/providers/index.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { ThemeProvider } from "next-themes";
+
+export default function Providers({ children }: {
+  children:
+  React.ReactNode
+}) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
