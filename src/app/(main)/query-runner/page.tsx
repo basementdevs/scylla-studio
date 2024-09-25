daef0a5f7ac2e80c50752001de8c1457f87baed3
matozinho <matozinhof.academico@gmail.com>
2024-09-25 17:37:04 -0300

    fix(package.json): update next and tailwindcss versions to the latest feat(query-runner/page.tsx): add QueryRunnerPage component to display query runner page feat(sidebar/admin-panel-layout.tsx): wrap main and footer in a div for better structure style(sidebar/collapse-menu-button.tsx): simplify button structure for CollapseMenuButton style(sidebar/footer.tsx): adjust footer styling for better positioning style(sidebar/menu.tsx): improve menu layout and link styling style(sidebar/sidebar.tsx): add gap between elements in Sidebar component feat(lib/menu-list.ts): add Query Runner menu item to getMenuList function
    Signed-off-by: matozinho <matozinhof.academico@gmail.com>

---
+++ src/app/(main)/query-runner/page.tsx
@@ -0,0 +1,53 @@
+import { ContentLayout } from "@scylla-studio/components/composed/sidebar/content-layout";
+
+export default function QueryRunnerPage() {
+  return (
+    <ContentLayout title="Query Runner">
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+      <h1>QueryRunnerPage</h1>
+    </ContentLayout>
+  );
+}
--- src/components/composed/sidebar/admin-panel-layout.tsx
+++ src/components/composed/sidebar/admin-panel-layout.tsx
@@ -18,22 +18,24 @@ export default function SidebarLayout({
   return (
     <>
       <Sidebar />
-      <main
-        className={cn(
-          "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300",
-          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
-        )}
-      >
-        {children}
-      </main>
-      <footer
-        className={cn(
-          "transition-[margin-left] ease-in-out duration-300",
-          sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
-        )}
-      >
-        <Footer />
-      </footer>
+      <div className="flex flex-col">
+        <main
+          className={cn(
+            "min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300 mb-14",
+            sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+          )}
+        >
+          {children}
+        </main>
+        <footer
+          className={cn(
+            "transition-[margin-left] ease-in-out duration-300",
+            sidebar?.isOpen === false ? "lg:ml-[90px]" : "lg:ml-72"
+          )}
+        >
+          <Footer />
+        </footer>
+      </div>
     </>
   );
 }
