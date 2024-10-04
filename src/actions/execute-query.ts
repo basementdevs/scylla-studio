411054910015ad5152eaea97b5cca0422a26885d
danielhe4rt <hey@danielheart.dev>
2024-10-04 14:38:27 -0300

    chore(connections): adding port and host validations

--- src/actions/execute-query.ts
+++ src/actions/execute-query.ts
@@ -13,9 +13,19 @@ export const executeQueryAction = actionClient
 			query: z.string(),
 			limit: z.number().optional(),
 			connection: z.object({
-				host: z.string(),
+				host: z.string().refine((value) => {
+					// Regex for matching localhost:port
+					const localhostRegex = /^localhost$/;
+					// Regex for matching IPv4 addresses
+					const ipv4Regex = /^(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\.(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})$/;
+					// Regex for matching domain-style address (example: node-0.aws-sa-east-1.1695b05c8e05b5237178.clusters.scylla.cloud)
+					const domainRegex = /^[a-zA-Z0-9-]+\.[a-zA-Z0-9.-]+$/;
+
+					return localhostRegex.test(value) || ipv4Regex.test(value) || domainRegex.test(value);
+				}),
 				id: z.number().optional(),
 				name: z.string().optional(),
+				port: z.number().optional(),
 				username: z.string().optional(),
 				password: z.string().optional(),
 				nodes: z.any().optional(),
@@ -50,13 +60,13 @@ export const executeQueryAction = actionClient
  */
 export const getSession = async (inputConnection: Partial<Connection>) => {
 	const connection = connections.find((c) => c.id === inputConnection.id);
-
+	console.log(`${inputConnection.host}:${inputConnection.port}`);
 	let session = connection?.session;
 
 	// if a connection was never open and stored, then create a new connection
 	if (!connection) {
 		const cluster = new Cluster({
-			nodes: [`${inputConnection.host}:9042`],
+			nodes: [`${inputConnection.host}:${inputConnection.port}`],
 		});
 		session = await cluster.connect();
 		connections.push({
@@ -74,7 +84,7 @@ export const getSession = async (inputConnection: Partial<Connection>) => {
 	// if the session is not valid, then create a new connection
 	if (!healthCheck) {
 		const cluster = new Cluster({
-			nodes: [`${inputConnection.host}:9042`],
+			nodes: [`${inputConnection.host}:${inputConnection.port}`],
 		});
 		const newSession = await cluster.connect();
 		connection.session = newSession;
--- src/lib/internal-db/connections.ts
+++ src/lib/internal-db/connections.ts
@@ -7,6 +7,7 @@ db.exec(`
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     host TEXT,
+    port TEXT,
     username TEXT,
     password TEXT,
     dc INTEGER,
@@ -17,31 +18,31 @@ db.exec(`
 export async function getAllConnections(): Promise<Connection[]> {
   return db
     .prepare(
-      "SELECT id, name, host, username, password, dc, nodes FROM connections"
+      "SELECT id, name, host, port, username, password, dc, nodes FROM connections"
     )
     .all() as Connection[];
 }
 
 export function addConnection(connection: Connection) {
-  const { name, host, username, password, dc, nodes } = connection;
+  const {name, host, port, username, password, dc, nodes} = connection;
   const stmt = db.prepare(`
-    INSERT INTO connections (name, host, username, password, dc, nodes)
-    VALUES (?, ?, ?, ?, ?, ?)
+    INSERT INTO connections (name, host, port, username, password, dc, nodes)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
   `);
-  stmt.run(name, host, username, password, 3, nodes);
+  stmt.run(name, host, port, username, password, 3, nodes);
 }
 
 export function updateConnectionById(
   connectionId: number,
   updatedConnection: Connection
 ) {
-  const { name, host, username, password, dc, nodes } = updatedConnection;
+  const {name, host, username, password, dc, nodes, port} = updatedConnection;
   const stmt = db.prepare(`
     UPDATE connections
-    SET name = ?, host = ?, username = ?, password = ?, dc = ?, nodes = ?
+    SET name = ?, host = ?, username = ?, password = ?, dc = ?, nodes = ?, port = ?
     WHERE id = ?
   `);
-  stmt.run(name, host, username, password, dc, nodes, connectionId);
+  stmt.run(name, host, username, password, dc, nodes, port, connectionId);
 }
 
 export function deleteConnectionById(connectionId: number) {
@@ -55,8 +56,9 @@ export type Connection = {
   id?: number;
   name: string;
   host: string;
-  username: string;
-  password: string;
+  port: number;
+  username?: string;
+  password?: string;
   dc?: string;
-  nodes: number;
+  nodes?: number;
 };
