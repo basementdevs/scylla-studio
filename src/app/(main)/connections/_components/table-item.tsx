230c37d9219a7c0f4433151dfd4f59759f4aea35
Daniel Boll <danielboll.academico@gmail.com>
2024-10-09 10:44:58 -0300

    chore: fix linting errors uncatch by `next lint`
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- src/app/(main)/connections/_components/table-item.tsx
+++ src/app/(main)/connections/_components/table-item.tsx
@@ -6,33 +6,35 @@ import React from "react";
 export default function TableLabel({
 	itemKey,
 	conn,
-}: { itemKey: string; conn: Connection }) {
+}: {
+	itemKey: string;
+	conn: Connection;
+}) {
 	const labels = {
 		Connected: {
-			text: "Connected",
-			variant: "primary",
+			text: "Connected" as const,
+			variant: "default" as const,
 		},
 		Refused: {
-			text: "Refused",
-			variant: "destructive",
+			text: "Refused" as const,
+			variant: "destructive" as const,
 		},
 		Offline: {
-			text: "Offline",
-			variant: "secondary",
+			text: "Offline" as const,
+			variant: "secondary" as const,
 		},
 	};
 
 	return (
 		<>
 			{itemKey === "status" && (
-				<Badge variant={labels[conn.status].variant}>
-					{" "}
-					{labels[conn.status].text}{" "}
+				<Badge variant={labels[conn.status ?? "Offline"].variant}>
+					{labels[conn.status ?? "Offline"].text}{" "}
 				</Badge>
 			)}
 			{itemKey === "password" && <Label>********</Label>}
 			{itemKey !== "status" && itemKey !== "password" && (
-				<Label>{conn[itemKey]} </Label>
+				<Label>{conn[itemKey as keyof Connection]} </Label>
 			)}
 		</>
 	);
