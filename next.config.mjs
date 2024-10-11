123daa78c151ed4486f86d0bb6098457839c57cc
Diego Reis <79876389+diegoreis42@users.noreply.github.com>
2024-10-11 09:35:43 -0300

    fix: contributors external images (#36)
    Address #34 issue
    
    Next.js's Image component must have all remote resources explicit
    declared in `next.config.js`
    [[1]](https://nextjs.org/docs/messages/next-image-unconfigured-host),
    moreover props `height` and `width` are required.

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@lambda-group/scylladb"],
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
