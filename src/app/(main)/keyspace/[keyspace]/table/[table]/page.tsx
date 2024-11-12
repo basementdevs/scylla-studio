5fb4e007ffb710f7e1b2ce420877b54ca879f5db
Lucas Rosa <lucassantosrosa51@gmail.com>
2024-11-12 15:02:14 -0300

    feat: add loading, connection and found validation to keyspaces tables page

--- src/app/(main)/keyspace/[keyspace]/table/[table]/page.tsx
+++ src/app/(main)/keyspace/[keyspace]/table/[table]/page.tsx
@@ -1,28 +1,69 @@
 "use client";
 
 import { ContentLayout } from "@scylla-studio/components/composed/sidebar/content-layout";
-import { Card } from "@scylla-studio/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@scylla-studio/components/ui/card";
+import { Skeleton } from "@scylla-studio/components/ui/skeleton";
 import { useLayout } from "@scylla-studio/contexts/layout";
+import { KeyspaceConnectionNotFound } from "../../_components/keyspace-connection-not-found";
 import TableGeneralInfo from "./_components/table-general-info";
 import TableStructure from "./_components/table-structure";
 
 export default function TableInfo({
   params: { keyspace, table },
-}: { params: { keyspace: string; table: string } }) {
-  const { betterKeyspaces } = useLayout();
+}: {
+  params: { keyspace: string; table: string };
+}) {
+  const { betterKeyspaces, selectedConnection, loadingKeyspaces } = useLayout();
 
   const selectedKeyspace = Object.values(betterKeyspaces).find(
-    (value) => value.name === keyspace,
+    (value) => value.name === keyspace
   );
   const selectedTable = selectedKeyspace?.tables.get(table);
 
   return (
     <ContentLayout title={selectedTable?.tableName ?? "Table information"}>
       <div className="container mx-auto p-4 space-y-6">
-        <Card>
-          {selectedTable && <TableGeneralInfo table={selectedTable} />}
-          {selectedTable && <TableStructure table={selectedTable} />}
-        </Card>
+        {!selectedConnection && !loadingKeyspaces && (
+          <KeyspaceConnectionNotFound />
+        )}
+
+        {selectedConnection && (
+          <Card>
+            {loadingKeyspaces && (
+              <>
+                <CardHeader>
+                  <Skeleton className="w-full h-9" />
+                </CardHeader>
+                <CardContent>
+                  <div className="flex gap-6">
+                    <Skeleton className="flex-1 h-80" />
+                    <Skeleton className="h-80 w-28" />
+                  </div>
+                </CardContent>
+              </>
+            )}
+
+            {!selectedTable && !loadingKeyspaces && (
+              <>
+                <CardHeader>
+                  <CardTitle>Table not found!</CardTitle>
+                </CardHeader>
+              </>
+            )}
+
+            {selectedTable && !loadingKeyspaces && (
+              <>
+                <TableGeneralInfo table={selectedTable} />
+                <TableStructure table={selectedTable} />
+              </>
+            )}
+          </Card>
+        )}
       </div>
     </ContentLayout>
   );
